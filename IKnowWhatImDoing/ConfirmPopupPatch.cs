9fa194ddcfe01cc61a90ae108c8fa58391719c2a
18107 <18107@users.noreply.github.com>
2024-07-24 21:54:52 +0930

    Add files via upload

---
+++ IKnowWhatImDoing/ConfirmPopupPatch.cs
@@ -0,0 +1,33 @@
+﻿using Client.Player.Interactions;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Popup;
+using UnityEngine;
+
+namespace IKnowWhatImDoing
+{
+    [HarmonyPatch(typeof(ConfirmPopup))]
+    internal class ConfirmPopupPatch
+    {
+        private static readonly MethodInfo ConfirmedMethod = AccessTools.Method(typeof(ConfirmPopup), "Confirmed");
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Show", new Type[] { typeof(string), typeof(string), typeof(Action), typeof(Action), typeof(Sprite) })]
+        static void Show1(ConfirmPopup __instance)
+        {
+            ConfirmedMethod.Invoke(__instance, null);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Show", new Type[] { typeof(ContextInfo), typeof(Action), typeof(Action), typeof(Sprite) })]
+        static void Show2(ConfirmPopup __instance)
+        {
+            ConfirmedMethod.Invoke(__instance, null);
+        }
+    }
+}
---
+++ IKnowWhatImDoing/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace IKnowWhatImDoing
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Client;
+
+        public override string Author => "18107";
+
+        public override string Description => "Skips all confirmation popups";
+    }
+}
