0011e587c56fa87e7489246717a8497cc49f65c1
Chris Wall <porisius@gmail.com>
2024-09-07 14:52:05 -0400

    Update 0.27.0 - Update 5

--- IKnowWhatImDoing/BepInPlugin.cs
+++ IKnowWhatImDoing/BepInPlugin.cs
@@ -5,23 +5,18 @@ using System.Reflection;
 
 namespace IKnowWhatImDoing
 {
-    internal static class MyPluginInfo
-    {
-        internal const string PLUGIN_GUID = "id107.iknowwhatimdoing";
-        internal const string PLUGIN_NAME = "IKnowWhatI'mDoing";
-        internal const string PLUGIN_VERSION = "0.0.0";
-    }
-
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.USERS_PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     [BepInProcess("Void Crew.exe")]
-    [BepInDependency("VoidManager")]
+    [BepInDependency(VoidManager.MyPluginInfo.PLUGIN_GUID)]
     public class BepinPlugin : BaseUnityPlugin
     {
+
         internal static ManualLogSource Log;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "N/A")]
         private void Awake()
         {
             Log = Logger;
-            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MyPluginInfo.PLUGIN_GUID);
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
